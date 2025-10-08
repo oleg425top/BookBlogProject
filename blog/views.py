df63e944134138a5cfc6a848823c97b15458b424
oleg425top <oleg59152@gmail.com>
2025-10-08 13:57:25 +0500

    16. Выделение основ слов и ранжирование результатов

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView, CreateView
 from django.utils.text import slugify
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -166,9 +166,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query),
+            ).filter(search=search_query).order_by('-rank')
     context = {
                 'title':'Posts Search',
                 'form':form,
