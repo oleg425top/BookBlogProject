0f63f0863b9ae7b696d4dbb17ad8c2d4c2f6fd26
oleg425top <oleg59152@gmail.com>
2025-10-10 12:20:21 +0500

    19. Перенес шаблон Base в отдельную директорию

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'users',
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
@@ -64,7 +65,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
